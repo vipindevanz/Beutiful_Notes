269d6d2448651aaa5a1fc9e9ee5d9ca910d0e306
Vipin Dev <anzirish@gmail.com>
2024-04-03 10:56:55 +0530

    Create implementation of note repository

---
+++ app/src/main/java/com/pns/beutifulnotes/data/repositories/NoteRepositoryImpl.kt
@@ -0,0 +1,26 @@
+package com.pns.beutifulnotes.data.repositories
+
+import com.pns.beutifulnotes.data.data_source.NoteDao
+import com.pns.beutifulnotes.domain.model.Note
+import com.pns.beutifulnotes.domain.repositories.NoteRepository
+import kotlinx.coroutines.flow.Flow
+
+class NoteRepositoryImpl(
+    private val dao: NoteDao
+) : NoteRepository {
+    override fun getNotes(): Flow<List<Note>> {
+        return dao.getNotes()
+    }
+
+    override suspend fun getNoteById(id: Int): Note? {
+        return dao.getNoteById(id)
+    }
+
+    override suspend fun insertNote(note: Note) {
+        dao.insertNote(note)
+    }
+
+    override suspend fun deleteNote(note: Note) {
+        dao.deleteNote(note)
+    }
+}
