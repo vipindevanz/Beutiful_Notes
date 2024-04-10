e026b0ca16c67c27c8cf950bef179c9c709e072c
Vipin Dev <anzirish@gmail.com>
2024-04-10 07:55:01 +0530

    Apply MVVM architecture

--- app/src/main/java/com/pns/beutifulnotes/MainActivity.kt
+++ app/src/main/java/com/pns/beutifulnotes/MainActivity.kt
@@ -1,46 +1,17 @@
 package com.pns.beutifulnotes
 
 import android.os.Bundle
+import com.pns.beutifulnotes.navigation.AppNavHost
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
-import com.pns.beutifulnotes.ui.theme.BeautifulNotesTheme
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            BeautifulNotesTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background
-                ) {
-                    Greeting("Android")
-                }
-            }
+            AppNavHost()
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    BeautifulNotesTheme {
-        Greeting("Android")
     }
 }
